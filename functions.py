6f5c325739d893bdddf6793499549369c1e12bfa
Safwan Molla <mollasafwan468@gmail.com>
2024-10-20 12:41:25 +0530

    Implement an Add Button

--- functions.py
+++ functions.py
@@ -4,11 +4,9 @@ FILEPATH = 'todos.txt'
 def get_todo(filepath=FILEPATH):
     with open(filepath, 'r') as f:
         local_todos = f.readlines()
-
     return local_todos
 
 
 def write_todo(local_todo, filepath=FILEPATH):
     with open(filepath, 'w') as f:
         f.writelines(local_todo)
-
